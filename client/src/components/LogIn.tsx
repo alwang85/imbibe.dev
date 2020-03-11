79f21748e9fcef69d52d67c9e9a93d195108c67e
Alexander Wang <alwang85@gmail.com>
2020-03-10 21:53:41 -0400

    docs(readme): updated readme, minor subitem anchor fix

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import { Button } from 'semantic-ui-react'
+import get from 'lodash/get';
 
 import { AuthWrapper } from '../context/auth0-context';
 
 interface LogInProps {
+  isAuthenticated: boolean,
+  location: any,
   loginWithRedirect: any
 }
 
@@ -15,11 +18,17 @@ export class LogIn extends React.PureComponent<LogInProps, LogInState> {
   }
 
   render() {
+    // const origin = get(window, 'location.origin');
+    // const previousPath = get(this.props.location, 'state.from.pathname');
+    // const redirectObj = {
+    //   redirect_uri: `${origin}${previousPath}`,
+    // };
+    // console.log('redirectObj', redirectObj)
     return (
       <div>
         <h1>Please log in</h1>
 
-        <Button onClick={this.onLogin} size="huge" color="olive">
+        <Button onClick={() => this.onLogin()} size="huge" color="olive">
           Log in
         </Button>
       </div>
